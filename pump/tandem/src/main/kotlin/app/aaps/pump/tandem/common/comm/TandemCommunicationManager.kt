f45dbb99483e56c6a6085f543be8ab9e01d302f3
James Woglom <j@wogloms.net>
2025-12-15 23:47:02 -0500

    Move from single command active at once to in flight requests and responses

--- pump/tandem/src/main/kotlin/app/aaps/pump/tandem/common/comm/TandemCommunicationManager.kt
+++ pump/tandem/src/main/kotlin/app/aaps/pump/tandem/common/comm/TandemCommunicationManager.kt
@@ -62,12 +62,11 @@ class TandemCommunicationManager(
     lateinit var peripheral: BluetoothPeripheral
     var connected = false
     var errorConnecting = false
-    var commandRequestModeRunning = false
+    var commandRequestModeRunning: Boolean = false
+        get() { return inFlightRequests.isNotEmpty() }
 
     var dataStore: TandemUIDataStore = tandemDataStore
 
-    var commandRequest: Message? = null
-    var commandResponse: Message? = null
     var communicationListener : CommunicationListener? = null
         set(value) {  if (value==null)
             operationMode=OperationMode.StandardOperation
@@ -86,7 +85,7 @@ class TandemCommunicationManager(
 
     companion object {
         val TAG = LTag.PUMPBTCOMM
-        val COMMAND_TIMEOUT = 30 * 1000  // 30s (in ms) timeout
+        val COMMAND_TIMEOUT = 5 * 1000  // 5s (in ms) timeout
     }
 
 
@@ -178,6 +177,8 @@ class TandemCommunicationManager(
     }
 
 
+    val inFlightRequests = mutableSetOf<Message>()
+    val inFlightResponses = mutableSetOf<Message>()
 
     /**
      * Sends command to the pump, if driver is in preventConnect mode any messages will be ignored,
@@ -186,7 +187,7 @@ class TandemCommunicationManager(
     @Synchronized
     fun sendCommand(request: Message, forceSend: Boolean = false): Message? {
 
-        aapsLogger.error(TAG, "sendCommand: ${request.javaClass.simpleName}")  // TODO
+        aapsLogger.info(TAG, "sendCommand: ${request.javaClass.simpleName}")  // TODO
 
         operationMode = OperationMode.StandardOperation
 
@@ -203,31 +204,32 @@ class TandemCommunicationManager(
             }
         }
 
-        this.commandRequestModeRunning = true
-        this.commandRequest = request
-        this.commandResponse = null
-
+        synchronized(inFlightRequests) {
+            this.inFlightRequests.add(request)
+            sendCommand(peripheral, request)
+        }
         aapsLogger.info(LTag.PUMPCOMM, "Sending Request: [code=${request.opCode()},class=${request::class.simpleName}]")
-
-        sendCommand(peripheral, request)
 
         val timeoutTime = System.currentTimeMillis() + COMMAND_TIMEOUT;
 
-        while(commandRequestModeRunning) {
+        while (commandRequestModeRunning) {
 
-            if (commandResponse!=null) {
-                this.commandRequestModeRunning = false
+            val commandResponse = this.inFlightResponses.find { it.opCode() == request.responseOpCode }
+            if (commandResponse != null) {
+                this.inFlightRequests.remove(request)
+                this.inFlightResponses.remove(commandResponse)
                 return commandResponse
             }
 
-            pumpUtil.sleep(1000)
+            pumpUtil.sleep(100)
 
-            if (System.currentTimeMillis()>timeoutTime) {
+            if (System.currentTimeMillis()>=timeoutTime) {
                 aapsLogger.error(TAG, "Timeout for command ${request.javaClass.name} returning with null.")
-                return null
+                break
             }
         }
 
+        this.inFlightRequests.remove(request)
         return null
     }
 
@@ -346,14 +348,13 @@ class TandemCommunicationManager(
     }
 
     fun receiveMessageInStandardMode(message: Message) {
-        if (this.commandRequest==null) {
+        if (!this.commandRequestModeRunning) {
             aapsLogger.error(LTag.PUMPCOMM, "No Command Requested, but received message [code=${message.opCode()}]")
         } else {
-            aapsLogger.info(LTag.PUMPCOMM, "ReceivedMessage [code=${message.opCode()},expected=${commandRequest!!.getResponseOpCode()},class=${message::class.simpleName}]")
-
-            if (message.opCode() == commandRequest!!.getResponseOpCode()) {
+            val matchingRequest = inFlightRequests.find { it.responseOpCode == message.opCode() }
+            if (matchingRequest != null) {
                 aapsLogger.info(LTag.PUMPCOMM, "Response received [code=${message.opCode()},class=${message::class.simpleName}]")
-                this.commandResponse = message
+                this.inFlightResponses.add(message)
             } else {
                 if (message is ApiVersionResponse) {
                     this.apiVersionResponseReceived = true
