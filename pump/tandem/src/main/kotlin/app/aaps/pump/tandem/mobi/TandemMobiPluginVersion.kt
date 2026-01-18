7210ace5f71990af778e2bb4c15f86c85c646ed7
andy.rozman <andy.rozman@beyondnow.com>
2026-01-18 14:53:45 +0000

    0.7.8.0 - just updated version after PR M4 from jwoglom (Pairing implementation)

--- pump/tandem/src/main/kotlin/app/aaps/pump/tandem/mobi/TandemMobiPluginVersion.kt
+++ pump/tandem/src/main/kotlin/app/aaps/pump/tandem/mobi/TandemMobiPluginVersion.kt
@@ -7,7 +7,7 @@ class TandemMobiPluginVersion {
     val devVersion = "3.4.0.0-dev (04.01.2026)"
 
     val pumpX2Version = BuildConfig.PUMPX2_VERSION
-    val tandemModuleVersion = "v0.7.7.0"
+    val tandemModuleVersion = "v0.7.8.0"
 
     companion object {
         @JvmStatic
